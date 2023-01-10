27374a5ba30d1072679eefaa048f611664d023cb
Zehra <aydin.fzehra@gmail.com>
2023-01-10 18:03:44 +0300

    change data directory and add svg files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import { Main } from "./components/Main";
 import { Sidebar } from "./components/Sidebar";
 import { useGlobal } from "./context/GlobalContext";
-import data from "../public/data.json";
+import data from "../public/assets/data/data";
 
 function App() {
   const { value, setValue } = useGlobal();
--- src/components/Main/Container/Aside/Sync/SyncImg/index.jsx
+++ src/components/Main/Container/Aside/Sync/SyncImg/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./index.module.css";
-import SyncImage from "../../../../../../assets/sync.svg";
+import SyncImage from "/assets/sync.svg";
 
 export function SyncImg() {
   return <img src={SyncImage} alt="" className={styles.svg} />;
